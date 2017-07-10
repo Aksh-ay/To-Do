d4e614d4b15d8e7f0d704acd92ff34d487b09020
Aksh-ay <akshayverma154@gmail.com>
2017-07-11 04:52:26 +0530

    after cardview

--- app/src/main/java/com/example/asus/todoclass2/MainActivity.java
+++ app/src/main/java/com/example/asus/todoclass2/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.DialogInterface;
@@ -78,6 +80,15 @@ public class MainActivity extends AppCompatActivity implements OnCheckBoxClicked
         });
 
         updateExpenseList();
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
     }
 
