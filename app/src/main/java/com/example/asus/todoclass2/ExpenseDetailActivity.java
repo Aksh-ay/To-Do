7a262cb6597933459c0998b94888f0d013f4d67f
Aksh-ay <akshayverma154@gmail.com>
2017-07-11 03:31:10 +0530

    before CardView

--- app/src/main/java/com/example/asus/todoclass2/ExpenseDetailActivity.java
+++ app/src/main/java/com/example/asus/todoclass2/ExpenseDetailActivity.java
@@ -1,13 +1,18 @@
 package com.example.asus.todoclass2;
 
+import android.app.DatePickerDialog;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class ExpenseDetailActivity extends AppCompatActivity {
 
@@ -17,6 +22,8 @@ public class ExpenseDetailActivity extends AppCompatActivity {
     EditText titleTextView;
     EditText categoryTextView;
     EditText priceTextView;
+    EditText dateEditText;
+    long date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,8 +80,48 @@ public class ExpenseDetailActivity extends AppCompatActivity {
             }
         });
 
+        // Steps for Date picker
+        // Will show Date picker dialog on clicking edit text
+        dateEditText = (EditText) findViewById(R.id.expenseDetailDateTextView);
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar newCalendar = Calendar.getInstance();
+                int month = newCalendar.get(Calendar.MONTH);  // Current month
+                int year = newCalendar.get(Calendar.YEAR);   // Current year
+                showDatePicker(ExpenseDetailActivity.this, year, month, 1);
+            }
+        });
 
 
+
+    }
+
+
+    public void showDatePicker(Context context, int initialYear, int initialMonth, int initialDay) {
+
+        // Creating datePicker dialog object
+        // It requires context and listener that is used when a date is selected by the user.
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    //This method is called when the user has finished selecting a date.
+                    // Arguments passed are selected year, month and day
+                    @Override
+                    public void onDateSet(DatePicker datepicker, int year, int month, int day) {
+
+                        // To get epoch, You can store this date(in epoch) in database
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, month, day);
+                        date = calendar.getTime().getTime();
+                        // Setting date selected in the edit text
+                        dateEditText.setText(day + "/" + (month + 1) + "/" + year);
+                    }
+                }, initialYear, initialMonth, initialDay);
+
+        //Call show() to simply show the dialog
+        datePickerDialog.show();
 
     }
 
